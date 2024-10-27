1f1a68a1f8c12b4bf97a4cbc1630d32c7d962d59
Alasdair Moloney <s3785526@student.rmit.edu.au>
2024-10-28 05:49:31 +1100

    fix: particle animation is paused when unfocused (no change when dt > 0.5s)

--- js/particle-background.js
+++ js/particle-background.js
@@ -2,26 +2,11 @@
 
 import { Point, Vec2d } from "./Vec2d.js";
 import { rand } from "./util.js";
+import Config from "./background-config.js";
 import Debug from "./debug.js";
 
 const canvas = document.getElementById("particleCanvas");
 const ctx = canvas.getContext("2d");
-
-const NO_OF_PARTICLES = 2500;
-const MIN_PARTICLE_SIZE = 0.5;
-const MAX_PARTICLE_SIZE = 1;
-const PARTICLE_INIT_SPEED_Y_MIN = 0.005;
-const PARTICLE_INIT_SPEED_Y_MAX = 0.02;
-
-// interactive
-const IMPACT_RADIUS = 50;
-const BURST_SPEED_X = 0.2;
-const BURST_SPEED_Y = 0.2;
-const X_DECELARATION = 0.99;
-const Y_DECELARATION = 0.99;
-const X_TOLERANCE = 0.00001;
-const Y_TOLERANCE = 0.00001;
-const INTERPOLATION_FACTOR = 0.01;
 
 let lastTime = document.timeline.currentTime;
 
@@ -34,8 +19,8 @@ class Particle {
     constructor(x, y) {
         this.coords = new Point(x,y);
         this.direction = new Vec2d(Vec2d.Y_UNIT_VECTOR);
-        this.size = rand(MIN_PARTICLE_SIZE, MAX_PARTICLE_SIZE);
-        this.initialSpeedY = rand(PARTICLE_INIT_SPEED_Y_MIN, PARTICLE_INIT_SPEED_Y_MAX);
+        this.size = rand(Config.MIN_PARTICLE_SIZE, Config.MAX_PARTICLE_SIZE);
+        this.initialSpeedY = rand(Config.PARTICLE_INIT_SPEED_Y_MIN, Config.PARTICLE_INIT_SPEED_Y_MAX);
         this.speedY = this.initialSpeedY;
         this.speedX = 0;
         this.color = "rgba(255, 255, 255, 0.7)";
@@ -67,16 +52,24 @@ class Particle {
 }
 
 function init() {
-    for(let i = 0; i < NO_OF_PARTICLES; ++i) {
+    for(let i = 0; i < Config.NO_OF_PARTICLES_1920; ++i) {
         particles.push(new Particle(Math.random() * canvas.width, Math.random() * canvas.height));
     }
 }
 
 function update(t) {
-    const dt = (t - lastTime);
+    let dt = (t - lastTime);
+
+    // handle refocus behaviour
+    // if dt will in a large animation jump, set dt = 0 for no change
+    if(dt >= Config.DT_THRESHOLD_MILLIS) {
+         dt = 0;
+    }
+
     lastTime = t;
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     Debug.debugParticle(particles[0]);
+    
     particles.forEach(particle => {
         particle.update(dt);
         particle.draw();
@@ -94,15 +87,15 @@ window.addEventListener("resize", () => {
 
 function updateSpeed(particle) {
     if(particle.speedX > 0) {
-        particle.speedX *= X_DECELARATION;
-        if(particle.speedX <= X_TOLERANCE) {
+        particle.speedX *= Config.X_DECELARATION;
+        if(particle.speedX <= Config.X_TOLERANCE) {
             particle.speedX = 0;
         }
     }
 
     if(particle.speedY > particle.initialSpeedY) {
-        particle.speedY *= Y_DECELARATION;
-        if(particle.speedY - particle.initialSpeedY <= Y_TOLERANCE) {
+        particle.speedY *= Config.Y_DECELARATION;
+        if(particle.speedY - particle.initialSpeedY <= Config.Y_TOLERANCE) {
             particle.speedY = particle.initialSpeedY;
         }
     }
@@ -111,11 +104,11 @@ function updateSpeed(particle) {
 
 function updateDirection(direction, target) {
     if(direction.x != target.x) {
-        direction.x += (target.x - direction.x) * INTERPOLATION_FACTOR;
+        direction.x += (target.x - direction.x) * Config.INTERPOLATION_FACTOR;
     }
 
     if(direction.y != target.y) {
-        direction.y += (target.y - direction.y) * INTERPOLATION_FACTOR;
+        direction.y += (target.y - direction.y) * Config.INTERPOLATION_FACTOR;
     }
 
 }
@@ -128,10 +121,10 @@ document.addEventListener("click", event => {
     particles.forEach(particle => {
         const dispVec = Vec2d.displacement(new Vec2d(x,y), particle.coords.toVec2d());
     
-        if(dispVec.magnitude <= IMPACT_RADIUS) {
+        if(dispVec.magnitude <= Config.IMPACT_RADIUS) {
             particle.direction = dispVec.getUnitVec();
-            particle.speedX = BURST_SPEED_X;
-            particle.speedY = BURST_SPEED_Y;
+            particle.speedX = Config.BURST_SPEED_X;
+            particle.speedY = Config.BURST_SPEED_Y;
         }
     });
 });
