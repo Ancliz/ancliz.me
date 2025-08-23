f0c7bc9a9d2693e3a6940b1cdeb336176e71218c
Ancliz <ancliz@ancliz.me>
2025-08-23 23:58:23 +1000

    fix: feedback on mylinks failing to fetch, display failure text

--- js/mylinks.js
+++ js/mylinks.js
@@ -1,6 +1,6 @@
 "use strict"
 
-import RequestBuilder, { HttpStatus } from "./requests-util.js";
+import RequestBuilder, { httpException, HttpStatus } from "./requests-util.js";
 
 class Entry {
     constructor(obj) {
@@ -20,39 +20,49 @@ async function getLinks() {
         return response.json().then(data => {
             return data.map(item => new Entry(item));
         });
-    } else {
-        console.error((await response.json()).message);
     }
-    return undefined;
+    throw new httpException(response.status, await response.json().message)
 }
 
 async function createLinks() {
-    const entries = await getLinks();
     const element = document.getElementById("my-links");
-    const fragment = document.createDocumentFragment();
+    element.innerHTML = "";
+    try {
+        const entries = await getLinks();
+        const fragment = document.createDocumentFragment();
+            
+        entries.forEach(entry => {
+            const anchor = document.createElement('a');
+            anchor.href = entry.url;
+            anchor.target = "_blank";
+            anchor.className = `flex items-center py-2 px-4 rounded-lg
+                                transition duration-300
+                                transform hover:text-white hover:scale-105
+                                text-gray-300 text-4xl font-bold`
+            const img = document.createElement("img");
+            img.src = entry.icon;
+            img.alt = entry.alt;
+            img.className = "h-8 w-8 mr-4";
+            anchor.appendChild(img);
+            anchor.appendChild(document.createTextNode(entry.text));
+            fragment.appendChild(anchor);
+        });
 
-    entries.forEach(entry => {
-        const anchor = document.createElement('a');
-        anchor.href = entry.url;
-        anchor.target = "_blank";
-        anchor.className = `flex items-center py-2 px-4 rounded-lg transition duration-300
-                            transform hover:text-white hover:scale-105
-                            text-gray-300 text-4xl font-bold`
-
-        const img = document.createElement("img");
-        img.src = entry.icon;
-        img.alt = entry.alt;
-        img.className = "h-8 w-8 mr-4";
-
-        anchor.appendChild(img);
-        anchor.appendChild(document.createTextNode(entry.text));
-        fragment.appendChild(anchor);
-    });
-
-    element.appendChild(fragment);
-
+        element.appendChild(fragment);
+    } catch(error) {
+        return { error, element }
+    }
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-   createLinks();
+document.addEventListener("DOMContentLoaded", async () => {
+    const result = await createLinks();
+    if(result?.error) {
+        console.error(result.error.message);
+        const message = document.createElement("div");
+        message.className = `flex flex-col justify-center items-center
+                                    text-center h-screen text-4xl font-bold
+                                    text-gray-300`;
+        message.innerHTML = "Failed to load page.<br>Please try again later.";
+        result.element.appendChild(message);
+    }    
 });
